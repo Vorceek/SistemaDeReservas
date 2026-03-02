c609f58a37efb5963be1031b750f48853fa53368
Vorceek <vorceek@gmail.com>
2026-03-02 17:40:30 -0300

    Controller HTTP para o Hotel

---
+++ SistemaDeReservas/Api/Controllers/HotelController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeReservas.Application.DTOs.Hoteis;
+using SistemaDeReservas.Application.DTOs.Users;
+using SistemaDeReservas.Application.Interfaces.Hoteis;
+using SistemaDeReservas.Application.Services;
+
+namespace SistemaDeReservas.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController(IHotelService hotelService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<ResponseHotelDto>> GetAllHotel()
+        {
+            return await hotelService.GetAllHotel();
+        }
+
+        [HttpPost]
+        public async Task<int> InsertAsync(CreateHotelDto hotel)
+        {
+            return await hotelService.InsertAsync(hotel);
+        }
+    }
+}
