76a025e703cbec2b1c731b3ddfa999bd873aafad
Vorceek <vorceek@gmail.com>
2026-03-02 17:37:25 -0300

    Novo HotelService

---
+++ SistemaDeReservas/Application/Services/Hoteis/HotelService.cs
@@ -0,0 +1,31 @@
+﻿using SistemaDeReservas.Application.DTOs.Hoteis;
+using SistemaDeReservas.Application.Interfaces.Hoteis;
+using SistemaDeReservas.Domain.Entities;
+
+namespace SistemaDeReservas.Application.Services.Hoteis
+{
+    public class HotelService(IHotelRepository repository) : IHotelService
+    {
+        public async Task<IEnumerable<ResponseHotelDto>> GetAllHotel()
+        {
+            var hotel = await repository.GetAllAsync();
+            return hotel.Select(h => new ResponseHotelDto
+            {
+                Id = h.Id,
+                Nome = h.Nome,
+                Cnpj = h.Cnpj
+            });
+        }
+
+        public async Task<int> InsertAsync(CreateHotelDto dto)
+        {
+            var hotel = new Hotel
+            {
+                Id = Guid.NewGuid(),
+                Nome = dto.Nome,
+                Cnpj = dto.Cnpj
+            };
+            return await repository.InsertAsync(hotel);
+        }
+    }
+}
