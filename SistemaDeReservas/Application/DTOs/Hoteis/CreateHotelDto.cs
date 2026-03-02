7292a2afba54d10fb5a2e4b6ff9d425ff71c4483
Vorceek <vorceek@gmail.com>
2026-03-02 17:39:48 -0300

    Novos DTOs Hotel

---
+++ SistemaDeReservas/Application/DTOs/Hoteis/CreateHotelDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaDeReservas.Application.DTOs.Hoteis
+{
+    public class CreateHotelDto
+    {
+        public required string Nome { get; set; }
+        public required string Cnpj { get; set; }
+    }
+}
---
+++ SistemaDeReservas/Application/DTOs/Hoteis/ResponseHotelDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaDeReservas.Application.DTOs.Hoteis
+{
+    public class ResponseHotelDto
+    {
+        public Guid Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Cnpj { get; set; }
+    }
+}
