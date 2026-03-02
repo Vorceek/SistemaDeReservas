4914680f51d5c98556901a2f82bb06614c8a0dda
Vorceek <vorceek@gmail.com>
2026-03-02 17:36:43 -0300

    Novo repositório Hotel

---
+++ SistemaDeReservas/Infraestructure/Repositories/HotelRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using SistemaDeReservas.Application.Interfaces.Hoteis;
+using SistemaDeReservas.Domain.Entities;
+using System.Data;
+
+namespace SistemaDeReservas.Infraestructure.Repositories
+{
+    public class HotelRepository : IHotelRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public HotelRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<IEnumerable<Hotel>> GetAllAsync()
+        {
+            var sql = "SELECT Id, Nome, Cnpj FROM Hoteis";
+            return await _connection.QueryAsync<Hotel>(sql);
+        }
+
+        public async Task<int> InsertAsync(Hotel hotel)
+        {
+            var sql = @"
+                INSERT INTO Hoteis (Id, Nome, Cnpj)
+                VALUES (@Id, @Nome, @Cnpj)";
+
+            return await _connection.ExecuteAsync(sql, hotel);
+        }
+    }
+}
