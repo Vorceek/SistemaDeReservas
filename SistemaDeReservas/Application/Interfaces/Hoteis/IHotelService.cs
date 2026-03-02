b2621b3317067dc1675851620dacb22004bdec73
Vorceek <vorceek@gmail.com>
2026-03-02 17:40:10 -0300

    Interface HotelService

---
+++ SistemaDeReservas/Application/Interfaces/Hoteis/IHotelService.cs
@@ -0,0 +1,10 @@
+﻿using SistemaDeReservas.Application.DTOs.Hoteis;
+
+namespace SistemaDeReservas.Application.Interfaces.Hoteis
+{
+    public interface IHotelService
+    {
+        Task<IEnumerable<ResponseHotelDto>> GetAllHotel();
+        Task<int> InsertAsync(CreateHotelDto dto);
+    }
+}
