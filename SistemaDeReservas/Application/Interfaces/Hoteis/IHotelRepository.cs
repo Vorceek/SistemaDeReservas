c61257d237cca5d30fc82dd039cc91968076f212
Vorceek <vorceek@gmail.com>
2026-03-02 17:39:20 -0300

    Inteface HotelRepository

---
+++ SistemaDeReservas/Application/Interfaces/Hoteis/IHotelRepository.cs
@@ -0,0 +1,10 @@
+﻿using SistemaDeReservas.Domain.Entities;
+
+namespace SistemaDeReservas.Application.Interfaces.Hoteis
+{
+    public interface IHotelRepository
+    {
+        Task<IEnumerable<Hotel>> GetAllAsync();
+        Task<int> InsertAsync(Hotel hotel);
+    }
+}
