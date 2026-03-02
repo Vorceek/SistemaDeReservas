ae3ecb68ecef426c3a794163c30e1f73f012655b
Vorceek <vorceek@gmail.com>
2026-03-02 16:49:32 -0300

    Nova entidade Quarto

---
+++ SistemaDeReservas/Domain/Entities/Quarto.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaDeReservas.Domain.Entities
+{
+    public class Quarto
+    {
+        public Guid Id { get; set; }
+        public required string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public int Capacidade { get; set; }
+        public decimal Diaria { get; set; }
+        public Guid HotelId { get; set; }
+    }
+}
