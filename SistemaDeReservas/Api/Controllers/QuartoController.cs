d5f0230c18261608bd61e354711ea0baf66be085
Vorceek <vorceek@gmail.com>
2026-03-03 09:49:14 -0300

    Controller HTTP para o Quarto

---
+++ SistemaDeReservas/Api/Controllers/QuartoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeReservas.Application.DTOs.Quartos;
+using SistemaDeReservas.Application.Interfaces.Quartos;
+
+namespace SistemaDeReservas.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuartoController(IQuartoService quartoService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<ResponseQuartoDto>> GetAllQuarto()
+        {
+            return await quartoService.GetAllQuarto();
+        }
+
+        [HttpPost]
+        public async Task<int> InsertAsync(CreateQuartoDto dto)
+        {
+            return await quartoService.InsertAsync(dto);
+        }
+    }
+}
