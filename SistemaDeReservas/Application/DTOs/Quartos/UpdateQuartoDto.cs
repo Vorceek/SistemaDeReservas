6eeb9ddbdd89d59e1891ba1f84177982313e681c
Vorceek <vorceek@gmail.com>
2026-03-03 09:47:12 -0300

    DTOs da entidade Quarto

---
+++ SistemaDeReservas/Application/DTOs/Quartos/UpdateQuartoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaDeReservas.Application.DTOs.Quartos
+{
+    public class UpdateQuartoDto
+    {
+        public required string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public int Capacidade { get; set; }
+        public decimal Diaria { get; set; }
+        public Guid HotelId { get; set; }
+    }
+}
