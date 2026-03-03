c8cd4cf44eabcd86f4bc6ee716d35f4854d0a889
Vorceek <vorceek@gmail.com>
2026-03-03 09:48:50 -0300

    Services e Repository da entidade Quarto

---
+++ SistemaDeReservas/Application/Services/Quartos/QuartoService.cs
@@ -0,0 +1,37 @@
+﻿using SistemaDeReservas.Application.DTOs.Quartos;
+using SistemaDeReservas.Application.Interfaces.Quartos;
+using SistemaDeReservas.Domain.Entities;
+
+namespace SistemaDeReservas.Application.Services.Quartos
+{
+    public class QuartoService(IQuartoRepository repository) : IQuartoService
+    {
+        public async Task<IEnumerable<ResponseQuartoDto>> GetAllQuarto()
+        {
+            var quarto = await repository.GetAllAsync();
+            return quarto.Select(q => new ResponseQuartoDto
+            {
+                Id = q.Id,
+                Nome = q.Nome,
+                Capacidade = q.Capacidade,
+                Quantidade = q.Quantidade,
+                Diaria = q.Diaria,
+                HotelId = q.HotelId
+            });
+        }
+
+        public async Task<int> InsertAsync(CreateQuartoDto dto)
+        {
+            var quarto = new Quarto
+            {
+                Id = Guid.NewGuid(),
+                Nome = dto.Nome,
+                Capacidade = dto.Capacidade,
+                Quantidade = dto.Quantidade,
+                Diaria = dto.Diaria,
+                HotelId = dto.HotelId
+            };
+            return await repository.InsertAsync(quarto);
+        }
+    }
+}
---
+++ SistemaDeReservas/Infraestructure/Repositories/QuartoRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using SistemaDeReservas.Application.Interfaces.Quartos;
+using SistemaDeReservas.Domain.Entities;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlTypes;
+
+namespace SistemaDeReservas.Infraestructure.Repositories
+{
+    public class QuartoRepository : IQuartoRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public QuartoRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<IEnumerable<Quarto>> GetAllAsync()
+        {
+            var sql = "SELECT Id, Nome, Quantidade, Capacidade, Diaria, HotelId FROM Quartos";
+            return await _connection.QueryAsync<Quarto>(sql);
+        }
+
+        public async Task<int> InsertAsync(Quarto quarto)
+        {
+            var sql = @"
+                INSERT INTO Quartos (Id, Nome, Quantidade, Capacidade, Diaria, HotelId)
+                VALUES (@Id, @Nome, @Quantidade, @Capacidade, @Diaria, @HotelId)";
+
+            return await _connection.ExecuteAsync(sql, quarto);
+        }
+    }
+}
