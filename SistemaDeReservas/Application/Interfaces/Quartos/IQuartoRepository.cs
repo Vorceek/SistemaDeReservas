3b0fafb8b3425a1815adef5359cd38a9e4122f8e
Vorceek <vorceek@gmail.com>
2026-03-03 09:48:09 -0300

    Intefaces Service e Repository para a entidade Quarto

---
+++ SistemaDeReservas/Application/Interfaces/Quartos/IQuartoRepository.cs
@@ -0,0 +1,10 @@
+﻿using SistemaDeReservas.Domain.Entities;
+
+namespace SistemaDeReservas.Application.Interfaces.Quartos
+{
+    public interface IQuartoRepository
+    {
+        Task<IEnumerable<Quarto>> GetAllAsync();
+        Task<int> InsertAsync(Quarto quarto);
+    }
+}
---
+++ SistemaDeReservas/Application/Interfaces/Quartos/IQuartoService.cs
@@ -0,0 +1,10 @@
+﻿using SistemaDeReservas.Application.DTOs.Quartos;
+
+namespace SistemaDeReservas.Application.Interfaces.Quartos
+{
+    public interface IQuartoService
+    {
+        Task<IEnumerable<ResponseQuartoDto>> GetAllQuarto();
+        Task<int> InsertAsync(CreateQuartoDto dto);
+    }
+}
