2f1982ccf9df5d7ad762840c0a1516474d3783a1
Vorceek <vorceek@gmail.com>
2026-03-06 09:32:06 -0300

    Renomeado de UserResponseDto.cs para ResponseUserDto. Alterado para manter o padrão do projeto

---
+++ SistemaDeReservas/Application/DTOs/Users/ResponseUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaDeReservas.Application.DTOs.Users
+{
+    public class UserResponseDto
+    {
+        public Guid Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Cpf { get; set; }
+    }
+}
