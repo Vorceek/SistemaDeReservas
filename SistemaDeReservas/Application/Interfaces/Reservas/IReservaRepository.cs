ccc964e3e791e2c545b62cc2fae9d15e4a983730
Vorceek <vorceek@gmail.com>
2026-03-06 09:17:42 -0300

    Interface para a entidade Reservas

---
+++ SistemaDeReservas/Application/Interfaces/Reservas/IReservaRepository.cs
@@ -0,0 +1,10 @@
+﻿using SistemaDeReservas.Domain.Entities;
+
+namespace SistemaDeReservas.Application.Interfaces.Reservas
+{
+    public interface IReservaRepository
+    {
+        Task<IEnumerable<Reserva>> GetAllAsync();
+        Task<int> InsertAsync(Reserva reserva);
+    }
+}
---
+++ SistemaDeReservas/Application/Interfaces/Reservas/IReservaService.cs
@@ -0,0 +1,10 @@
+﻿using SistemaDeReservas.Application.DTOs.Reservas;
+
+namespace SistemaDeReservas.Application.Interfaces.Reservas
+{
+    public interface IReservaService
+    {
+        Task<IEnumerable<ResponseReservaDto>> GetAllReserva();
+        Task<int> InsertAsync(CreateReservaDto dto);
+    }
+}
