a7e8bc8ab49f022a0ca51592ce95914eb0370739
Vorceek <vorceek@gmail.com>
2026-03-06 09:18:22 -0300

    ReservaRepository para a entidade Reserva

---
+++ SistemaDeReservas/Infraestructure/Repositories/ReservaRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using SistemaDeReservas.Application.Interfaces.Reservas;
+using SistemaDeReservas.Domain.Entities;
+using System.Data;
+
+namespace SistemaDeReservas.Infraestructure.Repositories
+{
+    public class ReservaRepository : IReservaRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ReservaRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<IEnumerable<Reserva>> GetAllAsync()
+        {
+            var sql = "SELECT Id, HotelId, QuartoId, UserId, CheckIn, CheckOut FROM Reservas";
+            return await _connection.QueryAsync<Reserva>(sql);
+        }
+
+        public async Task<int> InsertAsync(Reserva reserva)
+        {
+            var sql = @"
+                INSERT INTO Reservas (Id, HotelId, QuartoId, UserId, CheckIn, CheckOut)
+                VALUES (@Id, @HotelId, @QuartoId, @UserId, @CheckIn, @CheckOut)";
+
+            return await _connection.ExecuteAsync(sql, reserva);
+        }
+    }
+}
