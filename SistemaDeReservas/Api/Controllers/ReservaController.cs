c0ad3ba2e6961186b1cf41a3c11d44bf4073ef33
Vorceek <vorceek@gmail.com>
2026-03-06 09:18:55 -0300

    Service e Controller para a entidade Reservas

---
+++ SistemaDeReservas/Api/Controllers/ReservaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeReservas.Application.DTOs.Reservas;
+using SistemaDeReservas.Application.Interfaces.Reservas;
+
+namespace SistemaDeReservas.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservaController(IReservaService reservaService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<ResponseReservaDto>> GetAllReserva()
+        {
+            return await reservaService.GetAllReserva();
+        }
+
+        [HttpPost]
+        public async Task<int> InsertAsync(CreateReservaDto reserva)
+        {
+            return await reservaService.InsertAsync(reserva);
+        }
+    }
+}
---
+++ SistemaDeReservas/Application/Services/Reservas/ReservaService.cs
@@ -0,0 +1,38 @@
+﻿using SistemaDeReservas.Application.DTOs.Reservas;
+using SistemaDeReservas.Application.Interfaces.Reservas;
+using SistemaDeReservas.Domain.Entities;
+
+namespace SistemaDeReservas.Application.Services.Reservas
+{
+    public class ReservaService(IReservaRepository repository) : IReservaService
+    {
+
+        public async Task<IEnumerable<ResponseReservaDto>> GetAllReserva()
+        {
+            var reserva = await repository.GetAllAsync();
+            return reserva.Select(r => new ResponseReservaDto
+            {
+                Id = r.Id,
+                HotelId = r.HotelId,
+                QuartoId = r.QuartoId,
+                UserId = r.UserId,
+                CheckIn = r.CheckIn,
+                CheckOut = r.CheckOut
+            });
+        }
+
+        public async Task<int> InsertAsync(CreateReservaDto dto)
+        {
+            var reserva = new Reserva
+            {
+                Id = Guid.NewGuid(),
+                HotelId = dto.HotelId,
+                QuartoId = dto.QuartoId,
+                UserId = dto.UserId,
+                CheckIn = dto.CheckIn,
+                CheckOut = dto.CheckOut
+            };
+            return await repository.InsertAsync(reserva);
+        }
+    }
+}
